345a0c915235b72bfd40f5be8e7d750734b9652d
Mateus Bassi <mateus.s.bassi@gmail.com>
2023-11-20 18:51:59 -0300

    Ajustes e README

---
+++ README.md
@@ -0,0 +1,28 @@
+1-Layout:
+
+    Foi feito utilizando a função grid com um div container.
+    Foram utilizadas imagens na main para servir como menu.
+    Foi utilizado a função hover para dar um toque especial.
+    Existe um mini Sumário com os dados 'home', 'pedidos em andamento' e 'cadastrar'.
+
+2-Paginas:
+    Home: É a parte principal do sistema (index.html), onde está localizado o menu.
+
+    Pedidos em andamento: Assim que um pedido é cadastrado, ele é adicionado a uma tabela. Aqui é utilizado o recurso de loop "for" (Foreat) para exibir os pedidos.
+
+    Cadastrar: Nesta página, é utilizado uma função para salvar os dados na tabela de "pedidos em andamento".
+
+3-Utilidades:
+
+    IF: Verifica se já existe um "c".
+
+    Foreat: Utilizado na página 'tabela.html' para percorrer os pedidos e exibi-los na tabela.
+
+    Função: Utilizada no processo de cadastro para salvar os dados na tabela de "pedidos em andamento".
+
+4-Mais Informações:
+
+    projeto.js: Responsável pela parte do formulário.
+    Tem javascript em cada pagina tambem
+
+    index.css: Responsável pelo layout de todas as páginas.
--- projeto.js
+++ projeto.js
@@ -2,37 +2,35 @@
         const nome = document.getElementById('nome');
         const telefone = document.getElementById('telefone');
         const numero = document.getElementById('numero');
-        const pizza = document.getElementById('pizza'); // Adicione o ID correspondente ao select da pizza
-        const coca = document.getElementById('coca'); // Adicione o ID correspondente ao select da bebida
+        const pizza = document.getElementById('pizza'); 
+        const coca = document.getElementById('coca');
         const tabela = document.getElementById('tabela').getElementsByTagName('tbody')[0];
         
-        form.addEventListener('submit', function salvardados(event) {
-            event.preventDefault(); // Evita o comportamento padrão do formulário
-        
-            // Obter os valores do formulário
+        function salvardados(){ 
+            
+            // captura dados
             let novoNome = nome.value;
             let novoTelefone = telefone.value;
             let novoNumero = numero.value;
             let novoPizza = pizza.value;
             let novaCoca = coca.value;
         
-            // Criar uma nova linha na tabela
+            // Criar linha tabela
             let newRow = tabela.insertRow();
-        
-            // Adicionar células à nova linha
             let cellNome = newRow.insertCell(0);
             let cellTelefone = newRow.insertCell(1);
             let cellNumero = newRow.insertCell(2);
             let cellPizza = newRow.insertCell(3);
             let cellCoca = newRow.insertCell(4);
         
-            // Adicionar os valores do formulário às células
             cellNome.innerHTML = novoNome;
             cellTelefone.innerHTML = novoTelefone;
             cellNumero.innerHTML = novoNumero;
             cellPizza.innerHTML = novoPizza;
             cellCoca.innerHTML = novaCoca;
-        
-            // Limpar o formulário após adicionar à tabela
+            
+            localStorage.setItem('index', JSON.stringify(index));
+            window.location.href= "./index.html";
+
             form.reset();
-        });
+        }
